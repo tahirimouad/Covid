bf7f17d364cc083c5dd4b381e52aa4c55871afbb
oubtou mohamed <oubtou@hotmail.com>
2020-10-15 23:53:49 +0100

    terms activity

--- app/src/main/java/com/example/covtest/terms.java
+++ app/src/main/java/com/example/covtest/terms.java
@@ -6,19 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class terms extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_terms);
     }
 
-    public void start_test(View view){
-        Intent intent = new Intent(this, terms.class);
+    public void go(View view){
+        Intent intent = new Intent(this, TestActivity.class);
         startActivity(intent);
-    }
-
-    public void skip(View view){
     }
 }
