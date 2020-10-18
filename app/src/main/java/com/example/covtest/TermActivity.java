afc78b9c84d838fe2364e7f9f104272969b4388a
LoloziAnas <lolozianas1@gmail.com>
2020-10-18 03:25:29 +0100

    adding request task

--- app/src/main/java/com/example/covtest/TermActivity.java
+++ app/src/main/java/com/example/covtest/TermActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class terms extends AppCompatActivity {
+public class TermActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_terms);
+        setContentView(R.layout.activity_term);
     }
 
     public void go(View view){
