afa0a1b1d59b8d80956fc491b171293d81e4f4ca
LoloziAnas <lolozianas1@gmail.com>
2020-10-29 02:24:15 +0100

    Adding animation to activity_splash_screen

--- app/src/main/java/com/example/covtest/SplashScreen.java
+++ app/src/main/java/com/example/covtest/SplashScreen.java
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
     Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView imageViewLogo = findViewById(R.id.imv_logo);
+
+        Animation topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        Animation bottomAnim = AnimationUtils.loadAnimation(this,R.anim.buttom_animation);
+
+        imageViewLogo.setAnimation(topAnim);
+        imageViewLogo.setAnimation(bottomAnim);
+
         handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
